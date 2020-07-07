16aacff9fe9b66331e2a084bc46c4b97d9c79148
skeleton625 <chaos0824@naver.com>
2020-07-07 19:24:30 +0900

    Entity 오브젝트의 실시간 위치 이동 ComponentSystem 정의

---
+++ Assets/ECS_Test/Scripts/System/WaveSystem.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+/* Comopnent로 추가할 필요 없음 */
+public class WaveSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        /* 모든 Entity 들에 대한 함수 */
+        Entities.ForEach((ref Translation trans, ref UnitData data) =>
+        {
+            float zPos = data.amplitude * math.sin((float)(Time.ElapsedTime * data.UnitSpeed +
+                                                             trans.Value.x * data.xOffset + trans.Value.y * data.yOffset));
+            trans.Value = new float3(trans.Value.x, trans.Value.y, zPos);
+        });
+    }
+}
