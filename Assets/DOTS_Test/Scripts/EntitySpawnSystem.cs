b7ed72ba18b76ca8d0e20e9caf51afcd09177330
WaniFi <chaos0824@naver.com>
2020-07-09 01:44:39 +0900

    Entity 소환 ComponentSystem 구현

---
+++ Assets/DOTS_Test/Scripts/EntitySpawnSystem.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Collections;
+using UnityEngine;
+
+public class EntitySpawnSystem : ComponentSystem
+{
+    private int cnt = 0;
+    protected override void OnCreate()
+    {
+
+    }
+
+    protected override void OnUpdate()
+    {
+        if (cnt < 1000)
+        {
+            Entity spawnedEntity = EntityManager.Instantiate(PrefabConverter.prefabEntity);
+
+            float3 pos = UnityEngine.Random.insideUnitSphere * 64;
+            pos.y = 0;
+
+            EntityManager.SetComponentData(spawnedEntity, new Translation
+            {
+                Value = pos
+            });
+            ++cnt;
+        }
+    }
+}
