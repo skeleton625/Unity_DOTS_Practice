c519f917699dc46d4a803bf751835ca0993217a3
WaniFi <chaos0824@naver.com>
2020-07-09 01:44:12 +0900

    GameObject Prefab을 Entity로 변경하는 스크립트 구현

---
+++ Assets/DOTS_Test/Scripts/PrefabConverter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+public class PrefabConverter : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public GameObject prefab;
+    public static Entity prefabEntity;
+    // IConvertGameObjectToEntity 인터페이스의 인터페이스 메소드
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        using (BlobAssetStore blobAssetStore = new BlobAssetStore())
+        {
+            Entity prefabEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(prefab,
+                                  GameObjectConversionSettings.FromWorld(dstManager.World, blobAssetStore));
+            PrefabConverter.prefabEntity = prefabEntity;
+        }
+    }
+}
