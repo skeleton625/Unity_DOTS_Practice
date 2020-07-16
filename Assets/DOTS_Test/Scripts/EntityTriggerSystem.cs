fbb183608d6c6d60c5819d3b26e271ba4aac5cca
skeleton625 <chaos0824@naver.com>
2020-07-16 20:54:24 +0900

    Entity 끼리의 Trigger 충돌 확인 스크립트 구현

---
+++ Assets/DOTS_Test/Scripts/EntityTriggerSystem.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Physics;
+using Unity.Physics.Systems;
+
+[UpdateAfter(typeof(EndFramePhysicsSystem))]
+public class EntityTriggerSystem : JobComponentSystem
+{
+    private BuildPhysicsWorld buildPhysicsWorld;
+    private StepPhysicsWorld stepPhysicsWorld;
+
+    private EndSimulationEntityCommandBufferSystem commandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
+        stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+        commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    [BurstCompile]
+    struct EntityTriggerSystemJob : ITriggerEventsJob
+    {
+        [ReadOnly] public ComponentDataFromEntity<TreeData> allTrees;
+        [ReadOnly] public ComponentDataFromEntity<ControllerData> allPlayers;
+
+        public EntityCommandBuffer entityCommandBuffer;
+
+        public void Execute(TriggerEvent triggerEvent)
+        {
+            Entity entityA = triggerEvent.Entities.EntityA;
+            Entity entityB = triggerEvent.Entities.EntityB;
+
+            if (allTrees.Exists(entityA) && allTrees.Exists(entityB))
+                return;
+            if(allTrees.Exists(entityA) && allPlayers.Exists(entityB))
+            {
+                Debug.Log(entityA.ToString() + " " + entityB.ToString());
+                entityCommandBuffer.DestroyEntity(entityA);
+            }
+            else if(allPlayers.Exists(entityA) && allTrees.Exists(entityB))
+            {
+                entityCommandBuffer.DestroyEntity(entityB);
+            }
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new EntityTriggerSystemJob();
+
+        job.allTrees = GetComponentDataFromEntity<TreeData>(true);
+        job.allPlayers = GetComponentDataFromEntity<ControllerData>(true);
+        job.entityCommandBuffer = commandBufferSystem.CreateCommandBuffer();
+
+        JobHandle jobHandle = job.Schedule(stepPhysicsWorld.Simulation, ref buildPhysicsWorld.PhysicsWorld, inputDeps);
+
+        commandBufferSystem.AddJobHandleForProducer(jobHandle);
+        return jobHandle;
+    }
+}
